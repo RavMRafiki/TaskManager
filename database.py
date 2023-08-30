604bcfe618bc6f94796704a5fb11fd07c6c92937
Areem <98659406+RavMRafiki@users.noreply.github.com>
2023-08-30 20:27:02 +0200

    fix

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/task"
+SQLALCHEMY_DATABASE_URL = "postgres://default:Qu3Gl9NpZoct@ep-odd-sun-43129118-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
