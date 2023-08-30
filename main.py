eb283c79d24134c530fd4c16358324f49f07ae35
Areem <98659406+RavMRafiki@users.noreply.github.com>
2023-08-30 20:09:06 +0200

    init

---
+++ main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from model import Task
+from schema import task_schema
+from session import create_get_session
+
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Server is up and running!"}
+
+@app.get("/task", response_model=List[task_schema], status_code=200)
+async def read_tasks(db: Session = Depends(create_get_session)):
+   tasks = db.query(Task).all()
+   return tasks
+
+@app.post('/task', response_model = task_schema, status_code=201)
+async def create_task(task: task_schema, db: Session = Depends(create_get_session)):
+   new_task = Task(
+        task_name = task.task_name,
+        task_des = task.task_des,
+        created_by =task.created_by,
+        date_created = task.date_created,
+   )
+   db.add(new_task)
+   db.commit()
+
+   return new_task
+
+# @app.get("/task/{id}", response_model = task_schema, status_code=200)
+# async def get_task(id:int,db: Session = Depends(create_get_session)):
+#    task = db.query(Task).get(id)
+#    return task
+
+# @app.patch("/task/{id}", response_model = task_schema, status_code=200)
+# async def update_task(id:int, task:task_schema, db: Session = Depends(create_get_session)):
+#    db_task = db.query(Task).get(id)
+#    db_task.task_name = task.task_name
+#    db_task.task_des =  task.task_des
+#    db.commit()
+#    db.refresh(db_task)
+
+#    return db_task
+
+# @app.delete('/task/{id}', status_code=200)
+# async def delete_task(id:int, db: Session = Depends(create_get_session)):
+# 	db_task = db.query(Task).get(id)
+#     if not db_task:
+#     	raise HTTPException(status_code="404",detail="Task id does not exist")
+        
+# 	db.delete(db_task)
+#     db.commit()
+	
+#     return None
---
+++ model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base  = declarative_base()
+
+class Task(Base):
+    __tablename__ = "Tasks"		
+    id = Column(Integer, primary_key=True, index=True)		
+    task_name = Column(String(20))		
+    task_des = Column(String())		
+    created_by = Column(String(20))		
+    date_created = Column(String(15))
