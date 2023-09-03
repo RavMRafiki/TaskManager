f755b1effa61f7ec04e889b28392288d00ce9e2d
Areem <98659406+RavMRafiki@users.noreply.github.com>
2023-09-04 00:30:11 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgres://default:Qu3Gl9NpZoct@ep-odd-sun-43129118.eu-central-1.postgres.vercel-storage.com:5432/verceldb"
+SQLALCHEMY_DATABASE_URL = "postgresql://uu2z6538gcgk7emceidd:wSlJdLJ02Sxkx4ZK5dNbqR0AIVi7Uu@bjvr6fcz4godqoi9yo1p-postgresql.services.clever-cloud.com:5432/bjvr6fcz4godqoi9yo1p"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
